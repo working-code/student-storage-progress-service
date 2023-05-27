ef798ac6f69075a1bf5bb12694b5a85d0d99f9dc
Vlad Galushka <galushka.vp@gmail.com>
2023-05-27 16:01:31 +0700

    remove crud form

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    Request::enableHttpMethodParameterOverride();
-
-    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };

cd56cb955e7ffaf6797e458fe2128940fd4c49d8
Vlad Galushka <galushka.vp@gmail.com>
2023-05-31 20:08:16 +0700

    add sentry

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true, 'dev' => true],
 ];

83e9d894bc43128e5e15c6f697bcb8ce3fb62532
Vlad Galushka <galushka.vp@gmail.com>
2023-07-02 22:45:48 +0700

    prepare for tests

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Tests;
 
 /**
@@ -15,12 +16,33 @@ namespace App\Tests;
  * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
  *
  * @SuppressWarnings(PHPMD)
-*/
+ */
 class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+    public function authForUser(string $login): void
+    {
+        match ($login) {
+            'admin' => $this->amAdmin(),
+            'student' => $this->amStudent(),
+            'teacher' => $this->amTeacher(),
+            default => null,
+        };
+    }
+
+    public function amAdmin(): void
+    {
+        $this->amHttpAuthenticated('admin', 'admin');
+    }
+
+    public function amStudent(): void
+    {
+        $this->amHttpAuthenticated('student', 'student');
+    }
+
+    public function amTeacher(): void
+    {
+        $this->amHttpAuthenticated('teacher', 'teacher');
+    }
 }
